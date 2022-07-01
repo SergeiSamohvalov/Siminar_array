cbb00f8f74a509954f26084d0e469e0609929dc3
sergei sam <ssi1970@mail.ru>
2022-07-01 19:08:40 +0700

    Задача 15

---
+++ task_15/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+Console.Write("Введите порядковый номер дня недели от 1 до 7: ");
+Console.WriteLine();
+int numberWeekday = Convert.ToInt32(Console.ReadLine());
+
+while (numberWeekday < 8)
+{
+    if (numberWeekday <= 5)
+    {
+        Console.Write(" - это рабочий день недели.");
+        break;
+    }
+    else
+    {
+        if (numberWeekday >= 6) ;
+        {
+            Console.Write("- это выходной день недели.");
+            break;
+        };
+    }
+}
+

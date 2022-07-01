6f0c85ae2d8895819ebe1ffab7be0304c6c933ca
sergei sam <ssi1970@mail.ru>
2022-07-01 19:01:46 +0700

    Задача 10

---
+++ task_10/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+// Этот метод заполняет массив случайными числами
+void FillArray(int[] number)
+
+{
+    int length = number.Length;
+    int index = 0;
+    while (index < length)
+    {
+        number[index] = new Random().Next(1,3);
+        index++;
+    }
+}
+// Этот метод печатет массив
+void Printarray(int[] mass)
+{
+    int count = mass.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(mass[index].ToString());
+        index++;
+    }
+}
+
+int[] array = new int[3];
+
+FillArray(array);
+Printarray(array);
+
+Console.WriteLine();
+
+// Выводим второй элемент на печать
+Console.Write(array[1]);
